47cd08d09976b6ecd25a70493065936b8138877b
SA352283 <sai.vineelthamishetty@wipro.com>
2017-12-20 15:29:58 +0530

    Updated readme.md file

--- Executer.cmd
+++ Executer.cmd
@@ -4,17 +4,23 @@ REM Here the below path is given based on my local system it needs to be changed
 
 REM Downloader script downloads the file from the bse website and places to the required folder.
 
-python C:\Users\sa352283\Desktop\SimpleTask\Downloader.py
+python C:\Users\sai\Desktop\PythonProject\Downloader.py
 
 REM Extracter script extracts the downloaded zip file.
 
-python C:\Users\sa352283\Desktop\SimpleTask\Extracter.py
+python C:\Users\sai\Desktop\PythonProject\Extracter.py
 
 REM Loader script flushes the previous days data in the database and loads the todays data.
 
-python C:\Users\sa352283\Desktop\SimpleTask\Loader.py
+python C:\Users\sai\Desktop\PythonProject\Loader.py
 
 REM Move_To_Archive script moves the scripts downloaded in to the Archive location.
 
-python C:\Users\sa352283\Desktop\SimpleTask\Move_To_Archive.py
+python C:\Users\sai\Desktop\PythonProject\Move_To_Archive.py
+
+REM MyProj.py is the heart of this project.This files is responsible for the web server to run and server the search requests.
+
+python C:\Users\sai\Desktop\PythonProject\Website
+
+
 
